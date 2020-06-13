4047e95c30907e7ff66883caad6735bc37b2818f
iplaughlin <51142459+iplaughlin@users.noreply.github.com>
2020-06-12 21:22:53 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,30 +4,18 @@ Created on Mon May  4 15:22:27 2020
 
 @author: Ian
 """
-import sys
-sys.path.append(r"C:\Users\Ian\AnacondaProjects\GitHub\USAJobs")
-
 from urllib.parse import quote_plus
-
-
 import praw
 import pdb
 import re
 import os
 #import your secrets file
-from config_file import config
+
 #%%
 reply_text = 'test'
 reddit_bot = praw.Reddit('testbot')
 
 
-
-client_id = config['clientId'],
-                         client_secret = config['secret'], 
-                         #password = config['password'], 
-                         user_agent = 'testBot',)
-                         #username = config['user'])
-    
 subreddit = reddit_bot.subreddit("EmergencyManagement")
 for submission in subreddit.stream.submissions():
     if re.search(r'(Jobs Roundup Thread)', submission.title, re.IGNORECASE):
