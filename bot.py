65f45a17382ecdd21a447e3d7e5852926973f862
iplaughlin <51142459+iplaughlin@users.noreply.github.com>
2020-06-13 17:54:43 -0400

    updates

--- bot.py
+++ bot.py
@@ -4,42 +4,23 @@ Created on Mon May  4 15:22:27 2020
 
 @author: Ian
 """
-from urllib.parse import quote_plus
 import praw
-import pdb
-import re
-import os
-import datetime
 
 #%%
 
-def post_to_reddit(bot_to_use, title = '', posting_text = '')
+def submit_to_reddit(bot_to_use, sub_to_post_to, title = '', posting_text = ''):
+    reddit_bot = praw.Reddit(bot_to_use)
+    subreddit = reddit_bot.subreddit(sub_to_post_to)
+    subreddit.submit(title, selftext = posting_text)
+    for item in subreddit.new(limit = 5):
+        if item.title == title:
+            return item.url
+        else:
+            return False
 
+def post_to_reddit(bot_to_use, submission_url, posting_text):
+    reddit_bot = praw.Reddit(bot_to_use)
+    submission = reddit_bot.submission(url = submission_url)
+    submission.reply(posting_text)
 
-today = datetime.datetime.today()
-month = today.strftime('%B')
-year = today.year
-
-title = f'{month} {year} Jobs Roundup Thread'
-posting_text = """Please put each new job posting in a reply to the top-level post (i.e., this one);
- include a link; say where the job is located; and provide an optional short summary of the duties and pay scale (if known).
- One job per response, please, so we can clearly thread any subsequent discussion.
-All sectors are welcome; most of my own postings are likely to be in my own sector, higher education.
-
-Disclaimer: Unless otherwise specified, users who post jobs have no connection to any position or organization listed here,
-so please don't ask for referrals or gouge. This thread is intended as a clearinghouse only."""
-
-reddit_bot = praw.Reddit('EMJobsBot')
-
-subreddit = reddit_bot.subreddit("EmergencyManagement")
-
-subreddit.submit(title, selftext = posting_text)
-
-# for submission in subreddit.stream.submissions():
-#     if re.search(r'(Jobs Roundup Thread)', submission.title, re.IGNORECASE):
-#         if submission.title == 'May 2020 Jobs Roundup Thread':
-#             submission.reply(reply_text)
-
-# reply_template = '[let me google that for you](https://lmgtfy.com/q={})'
-# url_title = quote_plus()
 
